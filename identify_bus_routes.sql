0b5bad3c81f10d2a44f62289db94f71fcb8aec48
David5p <davidarcherqaz@gmail.com>
2026-09-02 13:23:07 +0200

    Update identify_bus_routes.sql

--- identify_bus_routes.sql
+++ identify_bus_routes.sql
@@ -1,1 +1,8 @@
-
+### Identify Bus routes ###
+SELECT 
+  route_type,
+  route_short_name, 
+  route_long_name
+FROM `sltraffik.stockholm_transport.routes`
+WHERE route_type = 700
+ORDER BY route_short_name;
