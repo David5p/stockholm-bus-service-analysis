27963a6074a12e04b6fc4be920192eab74f8003b
David5p <davidarcherqaz@gmail.com>
2026-09-02 13:44:50 +0200

    Delete identify_bus_routes.sql

--- identify_bus_routes.sql
+++
@@ -1,8 +0,0 @@
-### Identify Bus routes ###
-SELECT 
-  route_type,
-  route_short_name, 
-  route_long_name
-FROM `sltraffik.stockholm_transport.routes`
-WHERE route_type = 700
-ORDER BY route_short_name;
